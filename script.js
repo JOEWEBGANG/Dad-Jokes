4d7cda28f4ea0704e5bc0d5baedbc4aae6cad9c2
Zachary White <zachwhite2001@gmail.com>
2025-02-21 12:39:09 -0500

    add more custom functions to page

--- script.js
+++ script.js
@@ -18,3 +18,14 @@ generateJoke();
 
 // listeners
 jokeBtn.addEventListener('click', generateJoke);
+
+// scroll progress bar
+function updateScrollBar() {
+  let scrollTop = document.documentElement.scrollTop || document.body.scrollTop;
+  let scrollHeight = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+  let scrollPercent = (scrollTop / scrollHeight) * 100;
+
+  document.querySelector(".progress-bar").style.width = scrollPercent + "%";
+}
+
+window.addEventListener("scroll", updateScrollBar);
